9b22a34852db06af05c04f0a08051e8e3ad2aacd
Armaan Aravazhi <armaanaravazhi2007@gmail.com>
2025-09-28 18:40:28 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header.jsx'
+import Hero from './components/Hero.jsx'
+import About from './components/About.jsx'
+import Skills from './components/Skills.jsx'
+import Projects from './components/Projects.jsx'
+import Contacts from './components/Contacts.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  return (
+    <><Header /><Hero /><About /><Skills /><Projects /><Contacts /><Footer /></>
+  )
+}
+
+export default App;
